88ae4b49ac2c24e9f23bd5cc719f7d27ab14ab0c
TransReon <63791342+TransReon@users.noreply.github.com>
2020-10-16 09:21:50 +0800

    Update main.js
    修复bug

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var a = document.getElementById("input");
 var b = document.getElementById("par");
 function tip(){
-    var new = document.createElement("p");
-    new.innerHTML=a.value;
-    b.appendChild(new);
+    var newElement = document.createElement("p");
+    newElement.innerHTML=a.value;
+    b.appendChild(newElement);
 }
