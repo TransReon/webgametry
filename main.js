b1e49b7db27966dd9d4a383df95b4b2cca3ea684
TransReon <1795879118@qq.com>
2020-10-16 17:14:11 +0800

    页面整体修改

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-var a = document.getElementById("input");
-var b = document.getElementById("par");
-function tip(){
-    var newElement = document.createElement("p");
-    newElement.innerHTML=a.value;
-    b.appendChild(newElement);
-    a.value="";
+var gold = 0;
+var exp = 0;
+var recruit = 10;
+var goldtext = document.getElementById("gold");
+var exptext = document.getElementById("exp");
+var recruittext = document.getElementById("recruit");
+function updateStatus(){
+    goldtext.innerHTML = "金币： " + gold;
+    exptext.innerHTML = "经验： " + exp;
+    recruittext.innerHTML = "英雄帖： " + recruit;
 }
+
